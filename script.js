ca89e8af26aca53809bafc307cb9634f8edafdfb
garimagc7 <garimagc7@gmail.com>
2025-05-27 22:51:09 +0545

    almost done

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("Welcome to TicTacToe")
+let music = new Audio("utils/music.mp3")
+let turn : new Audio("utils/ting.mp3")
+let gameover = new Audio("utils/gameover.mp3")
+let turn = "X"
+
+const changeTurn = 
